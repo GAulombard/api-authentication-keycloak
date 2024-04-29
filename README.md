a3cc83b7c1df4b2f259bff2e01830d7468d777e7
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2024-04-29 18:56:05 +0200

    project updatedÃ

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # api-authentication-keycloak
+
+- run docker-compose up
+- configure your keycloak realm (port 8080)
+- run the app (port 8081) and test it
--- src/main/java/com/hodor/apiauthentication/controller/HelloController.java
+++ src/main/java/com/hodor/apiauthentication/controller/HelloController.java
@@ -1,19 +1,25 @@
 package com.hodor.apiauthentication.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/hello")
 public class HelloController {
 
-    @PostMapping("/hello")
+    @GetMapping
+    @PreAuthorize("hasRole('client_user')")
     public String hello() {
         return "Hello World - USER";
     }
 
-    @PostMapping("/hello2")
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('client_admin')")
     public String hello2() {
         return "Hello World - ADMIN";
     }
